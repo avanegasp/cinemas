0855019746d7e888e2e6a395fbacf7d18ea236ef
Angie <arco527@gmail.com>
2019-07-16 17:37:51 -0500

    Modal to create movies dont work

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::MoviesController < ApplicationController
   protect_from_forgery with: :null_session
-  
+
   def index
     @movies = Movie.all
   end
 
+  def new
+  respond_to do |format|
+    format.html
+    format.js
+  end
+end
 end
